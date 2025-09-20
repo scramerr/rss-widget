bb5341278f0146e04758494f4964bb3c6eeafb05
byter11 <mohsinshk4026@gmail.com>
2025-09-20 16:45:17 +0500

    #3 fix colors stuck after dark theme switch

--- app/src/main/java/com/byterdevs/rsswidget/ThemeUtils.kt
+++ app/src/main/java/com/byterdevs/rsswidget/ThemeUtils.kt
@@ -1,7 +1,10 @@
 package com.byterdevs.rsswidget
 
 import android.content.Context
+import android.content.res.ColorStateList
 import android.content.res.Configuration
+import android.graphics.Color
+import android.util.Log
 import android.view.ContextThemeWrapper
 import android.widget.RemoteViews
 import androidx.core.graphics.ColorUtils
@@ -20,11 +23,23 @@ object ThemeUtils {
         return ContextThemeWrapper(configurationContext, com.google.android.material.R.style.Theme_Material3_DynamicColors_DayNight)
     }
 
+    fun getBackgroundResource(transparency: Float): Int {
+        return when (transparency) {
+            in 0.0..0.1 -> R.drawable.background_0_1
+            in 0.1..0.2 -> R.drawable.background_0_2
+            in 0.2..0.3 -> R.drawable.background_0_3
+            in 0.3..0.4 -> R.drawable.background_0_4
+            in 0.4..0.5 -> R.drawable.background_0_5
+            in 0.5..0.6 -> R.drawable.background_0_6
+            in 0.6..0.7 -> R.drawable.background_0_7
+            in 0.7..0.8 -> R.drawable.background_0_8
+            in 0.8..0.9 -> R.drawable.background_0_9
+            in 0.9..1.0 -> R.drawable.background_1_0
+            else -> R.drawable.background_1_0
+        }
+    }
     fun setBgTransparency(context: Context, views: RemoteViews, viewName: Int, transparency: Float): RemoteViews {
-        val alpha = (transparency / 100 * 255).toInt()
-        val configurationContext = getThemedContextForWidget(context)
-        val colorBg = configurationContext.getColorResCompat(android.R.attr.colorBackground)
-        views.setInt(viewName, "setBackgroundColor", ColorUtils.setAlphaComponent(colorBg, alpha))
+        views.setInt(viewName, "setBackgroundResource", getBackgroundResource(transparency/100))
         return views
     }
 }
